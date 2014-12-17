5fcd0d8b62821f754eaac9eb8e38d8c1036af7f3
Андрей <mitzyk@mail.ru>
2014-12-17 19:17:26 +0300

    Added base elements. Added img. Implemented open file dialog.

--- CalculationOfProfit/Form1.Designer.cs
+++ CalculationOfProfit/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalculationOfProfit
 {
-    partial class Form1
+    partial class Frm_main
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -28,20 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_main));
+            this.Pb1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // Pb1
+            // 
+            this.Pb1.Image = ((System.Drawing.Image)(resources.GetObject("Pb1.Image")));
+            this.Pb1.Location = new System.Drawing.Point(104, 12);
+            this.Pb1.Name = "Pb1";
+            this.Pb1.Size = new System.Drawing.Size(363, 167);
+            this.Pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb1.TabIndex = 0;
+            this.Pb1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сохранить данные испытаний";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Открыть данные испытаний";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Frm_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 287);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Pb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Frm_main";
+            this.Text = "Расчет прибыли";
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Pb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CalculationOfProfit/Form1.cs
+++ CalculationOfProfit/Form1.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Windows.Forms;
 
 namespace CalculationOfProfit
 {
-    public partial class Form1 : Form
+    public partial class Frm_main : Form
     {
-        public Form1()
+        public Frm_main()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.InitialDirectory = Application.StartupPath + "\\Results";
+            openDialog.Filter = "xml file (*.xml)|*.xml";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openDialog.FileName;
+                XmlDocument document = new XmlDocument();
+                document.Load(fileName);
+            }
         }
     }
 }
